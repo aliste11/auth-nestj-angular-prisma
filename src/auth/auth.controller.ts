cb7c7a0073a7c34c81444b85e9c4dc54ac79f750
Igacio Aliste Toledo <aliste11toledo@gmail.com>
2025-09-03 13:12:55 -0400

    ADD: integrar autenticación JWT en el módulo de autenticación y actualizar el controlador y servicio para manejar el inicio de sesión y registro de usuarios

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 interface UserDto {
@@ -10,9 +10,10 @@ interface UserDto {
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Get('log-in')
-  logIn() {
-    return 'logIn';
+  @HttpCode(HttpStatus.OK)
+  @Post('log-in')
+  async logIn(@Body() user: UserDto) {
+    return await this.authService.logIn(user.email, user.password);
   }
 
   @Get('user')
@@ -20,6 +21,7 @@ export class AuthController {
     return this.authService.getUser();
   }
 
+  @HttpCode(HttpStatus.CREATED)
   @Post('sign-up')
   signUp(@Body() user: UserDto) {
     return this.authService.signUp(user.email, user.password);
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,48 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { compare } from 'bcrypt';
 import { encrytp } from 'src/libs/bcrypt';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly jwtService: JwtService
+  ) { }
   async getUser() {
     return await this.prismaService.user.findMany();
+  }
+
+  async logIn(email: string, password: string) {
+    try {
+      // si es que existe el correo
+      const user = await this.prismaService.user.findUnique({
+        where: {
+          email,
+        },
+      });
+
+      if (!user) {
+        throw new BadRequestException('credenciales invalidas');
+      }
+
+      const isPasswordMatch = await compare(password, user.password);
+      if (!isPasswordMatch) {
+        throw new BadRequestException('credenciales invalidas');
+      }
+      const { password: _, ...userWhithoutPassword } = user;
+
+      const payload = {
+        ...userWhithoutPassword,
+      };
+      const access_token = await this.jwtService.signAsync(payload);
+      return { access_token };
+    } catch (error) {
+      if (error instanceof BadRequestException) {
+        throw new InternalServerErrorException('Error al iniciar sesion');
+      }
+    }
   }
 
   async signUp(email: string, password: string) {
@@ -28,7 +64,9 @@ export class AuthService {
         },
       });
       const { password: _, ...userWhithoutPassword } = newUser;
-      return userWhithoutPassword;
+      const payload = { ...userWhithoutPassword };
+      const access_token = await this.jwtService.signAsync(payload);
+      return { access_token };
     } catch (error) {
       if (error instanceof BadRequestException) {
         throw error;
