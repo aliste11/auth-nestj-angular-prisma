6ed38f3eef9a429f78910a37a6692a3bd385ac6c
Igacio Aliste Toledo <aliste11toledo@gmail.com>
2025-09-02 23:14:11 -0400

    ADD: creacion de los controller module service para auth, configuracion de eslint para errores

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('auth')
+export class AuthController {
+  @Get('log-in')
+  logIn(){
+    return 'logIn'
+  }
+
+  @Get('sign-up')
+  signUp(){
+    return 'signUp'
+  }
+}
